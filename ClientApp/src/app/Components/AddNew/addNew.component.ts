1f26d8460ccc2d5e9b8a8e8678cc2bcc80e285a5
Jonah Latham <jonah.latham21@gmail.com>
2020-06-28 16:59:15 -0400

    code

---
+++ ClientApp/src/app/Components/AddNew/addNew.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+
+@Component({
+  selector: 'app-addNew',
+  templateUrl: './addNew.component.html',
+  styleUrls: ['./addNew.component.scss']
+})
+export class AddNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./Components/login/login.component";
 import { RegisterComponent } from "./Components/Register/register.component";
+import { HomeComponent } from "./Components/Home/home.component";
+import { AddNewComponent } from './Components/AddNew/addNew.component';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent }
+  { path: "register", component: RegisterComponent },
+  { path: "home", component: HomeComponent },
+  { path: "add", component: AddNewComponent },
 ];
 
 @NgModule({
